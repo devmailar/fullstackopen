31093d13cc6657f4cfb17104dc2c04ca9b992906
Leo Koivunen <leo.koivunen@iterator.fi>
2023-01-18 10:13:43 +0200

    Finish refactoring step 1.7

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h2>Statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>average: {average}</p>
+      <p>positive: {(good / total) * 100 || 0}%</p>
+    </div>
+  );
+};
+
+export default App;
