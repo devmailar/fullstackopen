0393b3e9f22b53f5718fb3fb61a81de058a8a1d2
Mailar <83369389+devmailar@users.noreply.github.com>
2023-02-08 15:44:36 +0200

    2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,31 +1,34 @@
-import { useState } from "react";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm.js";
-import Persons from "./components/Persons.js";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm.js';
+import Persons from './components/Persons.js';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(({ data }) => setPersons(data));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isDublicate = persons.find((person) => person.name === newName);
 
-    if (persons.find((person) => person.name === newName)) {
+    if (isDublicate) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
 
     setPersons([...persons, { name: newName, number: newNumber }]);
-    setNewName("");
-    setNewNumber("");
+
+    setNewName('');
+    setNewNumber('');
   };
 
   const filterPersons = persons.filter((person) =>
