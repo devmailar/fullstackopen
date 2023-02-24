ce9d2ef43f39a0186f2f7ab6fec254658cb0b1a1
Mailar <83369389+devmailar@users.noreply.github.com>
2023-02-24 11:01:00 +0200

    Refactor filter

--- osa2/puhelinluettelo/src/components/Filter.js
+++ osa2/puhelinluettelo/src/components/Filter.js
@@ -1,11 +1,14 @@
-const Filter = ({ searchTerm, setSearchTerm }) => (
-  <div>
-    filter shown with{" "}
-    <input
-      value={searchTerm}
-      onChange={(event) => setSearchTerm(event.target.value)}
-    />
-  </div>
-);
+const Filter = ({ filter, setFilter }) => {
+  const handleChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      filter shown with
+      <input value={filter} onChange={handleChange} />
+    </div>
+  );
+};
 
 export default Filter;
