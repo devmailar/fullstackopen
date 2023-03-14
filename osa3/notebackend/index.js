b1e557101201fb199f4d5af05043c8dd24f262b1
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-14 12:36:07 +0200

    Add lint

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -1,103 +1,103 @@
-const express = require("express");
-const cors = require("cors");
-const Note = require("./models/note");
-require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
+require('dotenv').config()
 
-const app = express();
-const PORT = process.env.PORT;
+const app = express()
+const PORT = process.env.PORT
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static("build"));
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 const reqLogger = (req, _res, next) => {
-  console.log("Method:", req.method);
-  console.log("Path:  ", req.path);
-  console.log("Body:  ", req.body);
-  console.log("---");
-  next();
-};
+  console.log('Method:', req.method)
+  console.log('Path:  ', req.path)
+  console.log('Body:  ', req.body)
+  console.log('---')
+  next()
+}
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(cors());
-app.use(express.json());
-app.use(reqLogger);
-app.use(express.static("build"));
+app.use(cors())
+app.use(express.json())
+app.use(reqLogger)
+app.use(express.static('build'))
 
-app.get("/api/notes", (req, res) => {
+app.get('/api/notes', (_req, res) => {
   Note.find({}).then((notes) => {
-    res.json(notes);
-  });
-});
+    res.json(notes)
+  })
+})
 
-app.post("/api/notes", (req, res, next) => {
-  const body = req.body;
+app.post('/api/notes', (req, res, next) => {
+  const body = req.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   note
     .save()
     .then((savedNote) => {
-      res.json(savedNote);
+      res.json(savedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/notes/:id", (req, res, next) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
     .then((note) => {
       if (note) {
-        res.json(note);
+        res.json(note)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/notes/:id", (req, res, next) => {
+app.delete('/api/notes/:id', (req, res, next) => {
   Note.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/notes/:id", (req, res, next) => {
-  const { content, important } = req.body;
+app.put('/api/notes/:id', (req, res, next) => {
+  const { content, important } = req.body
 
   Note.findByIdAndUpdate(
     req.params.id,
     { content, important },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedNote) => {
-      res.json(updatedNote);
+      res.json(updatedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
