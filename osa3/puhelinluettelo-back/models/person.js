0136db08283f562edac38d6f8a3c78bc1a493636
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-14 11:39:20 +0200

    3.20

--- osa3/puhelinluettelo-back/models/person.js
+++ osa3/puhelinluettelo-back/models/person.js
@@ -21,7 +21,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^(\d{8,}|\d{2,3}-\d+)$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
