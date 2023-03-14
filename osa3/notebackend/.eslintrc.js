4bf5bf58158a2a2f3ae8406c886a7af5cb8c8a1c
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-14 14:49:36 +0200

    Notebackend

--- osa3/notebackend/.eslintrc.js
+++ osa3/notebackend/.eslintrc.js
@@ -1,19 +1,40 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
   },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 'latest',
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
   },
-  rules: {
-    // ...
-    eqeqeq: 'error',
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
     'no-console': 0,
-  },
-};
+  }
+}
