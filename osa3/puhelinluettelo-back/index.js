ac1d264a88fc5e88dd4cc06f08a73fe116504027
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-14 12:46:55 +0200

    3.22

--- osa3/puhelinluettelo-back/index.js
+++ osa3/puhelinluettelo-back/index.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/person");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
 const app = express();
 
-require("dotenv").config();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("build"));
+app.use(express.static('build'));
 
 const errorHandler = (error, _req, res, next) => {
   console.error(error.message);
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
   }
 
@@ -25,14 +25,14 @@ const errorHandler = (error, _req, res, next) => {
 };
 
 const unknownEndpoint = (_req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+  res.status(404).send({ error: 'unknown endpoint' });
 };
 
-morgan.token("request-data", (req, _res) => JSON.stringify(req.body));
+morgan.token('request-data', (req, _res) => JSON.stringify(req.body));
 
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :request-data "
+    ':method :url :status :res[content-length] - :response-time ms :request-data '
   )
 );
 
@@ -40,7 +40,7 @@ app.use(
 //   res.send("Hello World");
 // });
 
-app.get("/info", (_req, res) => {
+app.get('/info', (_req, res) => {
   Person.find({}).then((result) => {
     res.send(
       `Phonebook has info for ${result.length} people <br> ${new Date()}`
@@ -48,13 +48,13 @@ app.get("/info", (_req, res) => {
   });
 });
 
-app.get("/api/persons", (_req, res) => {
+app.get('/api/persons', (_req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons);
   });
 });
 
-app.get("/api/persons/:id", async (req, res, next) => {
+app.get('/api/persons/:id', async (req, res, next) => {
   try {
     const person = await Person.findById(req.params.id);
     if (person) {
@@ -67,21 +67,21 @@ app.get("/api/persons/:id", async (req, res, next) => {
   }
 });
 
-app.delete("/api/persons/:id", (req, res, next) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((result) => {
       if (result) {
         return res.status(204).end();
       } else {
         return res.status(404).json({
-          error: "entry not found",
+          error: 'entry not found',
         });
       }
     })
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (req, res, next) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const { name, number } = req.body;
 
   Person.findByIdAndUpdate(
@@ -98,7 +98,7 @@ app.put("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res, next) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body;
 
   const person = new Person({
