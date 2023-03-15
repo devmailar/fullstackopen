baa4fd4309a2846cfd3b0535a9b59f50523fdcfd
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-15 10:43:37 +0200

    4.4 unit tests

--- osa3/bloglist/utils/list_helper.js
+++ osa3/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (listWithOneBlog) => {
+  return listWithOneBlog.reduce((total, blog) => {
+    total + blog.likes, 0;
+  });
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
