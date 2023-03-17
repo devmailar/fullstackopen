c1fa9d59666e9dd458807cc7e4c1a6048e34e64c
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-17 14:25:48 +0200

    4.9 tests

--- osa3/bloglist/app.js
+++ osa3/bloglist/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
+const app = express();
 
 mongoose.set('strictQuery', false);
 
--- osa3/bloglist/tests/bloglist_api.test.js
+++ osa3/bloglist/tests/bloglist_api.test.js
@@ -1,12 +1,51 @@
+const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
+const helper = require('./test_helper');
 
-test('return correct amount of blogs in json format', async () => {
-  let hi = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+const Blog = require('../models/blog');
 
-  console.log('return correct amount of blogs in json format\ndata:', hi.body);
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+describe('bloglist', () => {
+  test('all json blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toBeDefined();
+  });
+
+  test('correct amount of json blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('unique identifier property is named id', async () => {
+    const response = await api.get('/api/blogs');
+    const body = response.body;
+    body.forEach((blog) => {
+      expect(blog.id).toBeDefined();
+      expect(blog._id).toBeUndefined();
+    });
+  });
+
+  // Write a test that verifies that the unique identifier property of the blog posts is named id, by default the database names the property _id. Verifying the existence of a property is easily done with Jest's toBeDefined matcher.
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });
