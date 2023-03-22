359b1bcc1f444c007217e290acb719b9ec932922
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-22 10:49:27 +0200

    Finish populate practice

--- part3/practice-notes-back/controllers/notes.js
+++ part3/practice-notes-back/controllers/notes.js
@@ -1,21 +1,28 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
+
   response.json(notes);
 });
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id,
   });
 
   const savedNote = await note.save();
-  response.status(201).json(savedNote);
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
+  response.json(savedNote);
 });
 
 notesRouter.get('/:id', async (request, response) => {
@@ -41,10 +48,10 @@ notesRouter.put('/:id', (request, response, next) => {
   };
 
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
+    .then(updatedNote => {
       response.json(updatedNote);
     })
-    .catch((error) => next(error));
+    .catch(error => next(error));
 });
 
 module.exports = notesRouter;
