258bca5ea79b989b19aea51e980bccbb75a25732
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-24 12:34:04 +0200

    4.18 bloglist step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogsController');
 const usersRouter = require('./controllers/usersController');
+const loginRouter = require('./controllers/loginController');
 const config = require('./utils/config');
 const app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
