ed84a62fae5bd971032580e14124c343ffeba712
Mailar <83369389+devmailar@users.noreply.github.com>
2023-04-06 16:41:09 +0300

    5.17 bloglist e2e step1 fix

--- part5/bloglist-frontend/cypress/e2e/Blog.cy.js
+++ part5/bloglist-frontend/cypress/e2e/Blog.cy.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 describe('Blog app', function () {
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
 
