e753a742d0cb4802ccd0fa36ec18a631212e5e76
Mailar <83369389+devmailar@users.noreply.github.com>
2023-04-06 12:19:04 +0300

    5.15 bloglist step3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
+import { jest } from '@jest/globals'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
 import Blog from './Blog'
 
@@ -41,5 +42,23 @@ describe('Blog component', () => {
 
     expect(screen.getByTestId('url')).toHaveTextContent(blog.url)
     expect(screen.getByTestId('likes')).toHaveTextContent(`likes ${blog.likes}`)
+  })
+
+  test('calls the like event handler twice when the like button is clicked twice', () => {
+    const mockLikeBlog = jest.fn()
+
+    render(
+      <Blog blog={blog} likeBlog={mockLikeBlog} currentUser={{ id: '1' }} />
+    )
+
+    const detailsButton = screen.getByText('view')
+    fireEvent.click(detailsButton)
+
+    const likeButton = screen.getByRole('button', { name: 'like' })
+
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(mockLikeBlog.mock.calls).toHaveLength(2)
   })
 })
