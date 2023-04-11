b08adb7698452bbb392db4c5c721aab5ed58448f
Mailar <83369389+devmailar@users.noreply.github.com>
2023-04-11 11:32:27 +0300

    5.18 bloglist e2e step2

--- part5/bloglist-frontend/cypress/e2e/Blog.cy.js
+++ part5/bloglist-frontend/cypress/e2e/Blog.cy.js
@@ -1,12 +1,35 @@
 /* eslint-disable linebreak-style */
-describe('Blog app', function () {
-  beforeEach(function () {
+describe('Blog app', () => {
+  beforeEach(() => {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'testuser',
+      name: 'Test User',
+      password: 'password',
+    })
   })
 
-  it('Login form is shown', function () {
+  it('Login form is shown', () => {
     cy.contains('login').click()
     cy.get('form').should('be.visible')
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.contains('login').click()
+      cy.get('input[name="Username"]').type('testuser')
+      cy.get('input[name="Password"]').type('password')
+      cy.get('button[type="submit"]').click()
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.contains('login').click()
+      cy.get('input[name="Username"]').type('testuser')
+      cy.get('input[name="Password"]').type('wrongpassword')
+      cy.get('button[type="submit"]').click()
+      cy.contains('Wrong username or password')
+    })
   })
 })
