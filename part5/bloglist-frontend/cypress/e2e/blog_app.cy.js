ca8df2425fcb601d6689819921a2f1181bc93347
Mailar <83369389+devmailar@users.noreply.github.com>
2023-06-03 23:29:06 +0300

    5.21, 5.22, 5.23 excercises and fixes

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,108 +1,189 @@
+let userOne
+let userTwo
+
+let userOneBlog
+let userTwoBlog
+
 describe('Blog app', () => {
-  function resetDatabase() {
+  const resetDatabase = () => {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
   }
 
-  function createUser(user) {
+  const createUser = (user) => {
     cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
   }
 
   beforeEach(() => {
-    const user = {
+    resetDatabase()
+
+    userOne = {
       name: 'Mike Dylan',
-      username: 'mike',
-      password: 'password',
+      username: 'mikedylan',
+      password: 'test',
     }
 
-    resetDatabase()
-    createUser(user)
+    userTwo = {
+      name: 'Dave Dylan',
+      username: 'davedylan',
+      password: 'test',
+    }
+
+    createUser(userOne)
+    createUser(userTwo)
+
     cy.visit('http://localhost:3000')
   })
 
-  it('login form is shown', () => {
-    cy.contains('log in to application')
-    cy.contains('username')
-    cy.contains('password')
-    cy.contains('login').click()
-    cy.get('form').should('be.visible')
-  })
-
-  it('login succeeds with correct credentials', () => {
-    cy.get('input[name="Username"]').type('mike')
-    cy.get('input[name="Password"]').type('password')
-    cy.get('button[type="submit"]').click()
-    cy.contains('Mike Dylan logged in')
-    console.log('Success Login')
-  })
-
-  it('login fails with wrong credentials', () => {
-    cy.get('input[name="Username"]').type('mike')
-    cy.get('input[name="Password"]').type('wrong')
-    cy.get('button[type="submit"]').click()
-    cy.contains('Wrong username or password')
-    console.log('Failed Login')
-  })
-
-  describe('when logged in', () => {
-    beforeEach(() => {
-      cy.get('input[name="Username"]').type('mike')
-      cy.get('input[name="Password"]').type('password')
-      cy.get('button[type="submit"]').click()
-      cy.login({ username: 'mike', password: 'password' })
+  describe('log in to application', () => {
+    it('form shows', () => {
+      cy.get('form').should('be.visible')
     })
 
-    it('user can log out', () => {
+    it('succeeds with correct credentials', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
+      cy.get('button[type="submit"]').click()
+
+      cy.contains(`${userOne.name} logged in`)
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.logout()
+
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type('wrongpassword')
+      cy.get('button[type="submit"]').click()
+
+      cy.contains('Wrong username or password')
+    })
+  })
+
+  describe('logged in user', () => {
+    beforeEach(() => {
+      cy.login({ username: userOne.username, password: userOne.password })
+
+      userOneBlog = {
+        title: 'userOne Blog',
+        author: 'John Doe',
+        url: 'www.facebook.com',
+      }
+
+      cy.createBlog(userOneBlog)
+
+      cy.login({ username: userTwo.username, password: userTwo.password })
+
+      userTwoBlog = {
+        title: 'userTwo Blog',
+        author: 'John Doe',
+        url: 'www.amazon.com',
+      }
+
+      cy.createBlog(userTwoBlog)
+    })
+
+    it('A user can log out', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
+      cy.get('button[type="submit"]').click()
+
       cy.logout()
       cy.contains('log in to application')
       cy.contains('username')
       cy.contains('password')
     })
 
-    it('blog can be created', () => {
-      cy.contains('create new blog').click()
-
-      cy.get('input[name="title"]').type('Flynome')
-      cy.get('input[name="author"]').type('Kass')
-      cy.get('input[name="url"]').type('https://example.com/')
-
+    it('A blog can be created', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
       cy.get('button[type="submit"]').click()
 
-      cy.contains('Flynome')
-      cy.contains('Kass')
+      cy.contains('create new blog').click()
+      cy.get('input[name="title"]').type('Random Blog')
+      cy.get('input[name="author"]').type('Random Author')
+      cy.get('input[name="url"]').type('https://random.url.com')
+      cy.get('button[type="submit"]').click()
+
+      cy.contains('Random Blog')
     })
 
-    describe('existing', () => {
-      beforeEach(() => {
-        const blog = {
-          title: 'Flynome',
-          author: 'Kass',
-          url: 'https://example.com',
-          likes: 2,
-        }
+    it('A blog can be liked', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
+      cy.get('button[type="submit"]').click()
 
-        cy.createBlog(blog)
-      })
+      cy.contains(userOneBlog.title)
+        .parent()
+        .within(() => {
+          cy.contains('view').click()
+          cy.contains('like').click()
+          cy.contains('like').click()
+          cy.contains('likes 1')
+        })
+    })
 
-      it('blog can be liked', () => {
-        cy.contains('Flynome')
-          .parent()
-          .within(() => {
-            cy.contains('view').click()
-            cy.contains('like').click()
-          })
-      })
+    it('A user who didnt create the blog cant see remove button', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
+      cy.get('button[type="submit"]').click()
 
-      it('blog can be deleted', () => {
-        cy.contains('Flynome')
-          .parent()
-          .within(() => {
-            cy.contains('view').click()
-            cy.contains('like').click()
-            cy.contains('remove').click()
-          })
-        cy.get('html').should('not.contain', 'Flynome')
-        cy.get('html').should('not.contain', 'Kass')
-        cy.contains('blog removed')
+      cy.contains('userTwo Blog')
+        .parent()
+        .within(() => {
+          cy.contains('view').click()
+          cy.contains('remove').should('not.exist')
+        })
+    })
+
+    it('A user who created the blog can see remove button and delete it', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
+      cy.get('button[type="submit"]').click()
+
+      cy.contains(userOneBlog.title)
+        .parent()
+        .within(() => {
+          cy.contains('view').click()
+          cy.contains('remove').should('exist').click()
+        })
+    })
+
+    it('A blogs are ordered based on likes', () => {
+      cy.get('input[name="Username"]').type(userOne.username)
+      cy.get('input[name="Password"]').type(userOne.password)
+      cy.get('button[type="submit"]').click()
+
+      cy.contains(userTwoBlog.title)
+        .parent()
+        .find('button')
+        .contains('view')
+        .click()
+      cy.contains(userTwoBlog.title)
+        .parent()
+        .find('button')
+        .contains('like')
+        .click()
+
+      cy.contains(userOneBlog.title)
+        .parent()
+        .find('button')
+        .contains('view')
+        .click()
+      cy.contains(userOneBlog.title)
+        .parent()
+        .find('button')
+        .contains('like')
+        .click()
+      cy.contains(userOneBlog.title)
+        .parent()
+        .find('button')
+        .contains('like')
+        .click()
+
+      // Verify the order of blogs based on likes
+      cy.get('.container').should(($blogs) => {
+        expect($blogs).to.have.length(2)
+        expect($blogs.eq(0)).to.contain(userOneBlog.title)
+        expect($blogs.eq(1)).to.contain(userTwoBlog.title)
       })
     })
   })
