dbaaf360bbeb906d8f4dbb3d408a77f352f63d42
Mailar <83369389+devmailar@users.noreply.github.com>
2023-06-03 15:25:57 +0300

    Add morgan for dev logs

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+
 const mongoose = require('mongoose');
+
 const blogsRouter = require('./controllers/blogsController');
 const usersRouter = require('./controllers/usersController');
 const loginRouter = require('./controllers/loginController');
@@ -14,10 +17,19 @@ mongoose.connect(config.MONGODB_URI);
 
 console.info('connecting to', config.MONGODB_URI);
 
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.tokenExtractor);
+
+// log every request to the console
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+// your routes here
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
