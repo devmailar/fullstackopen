ed109848fe2c19598cd19bf31f8f3c742011b011
Mailar <83369389+devmailar@users.noreply.github.com>
2023-06-04 10:36:12 +0300

    6.2: unicafe revisited, step2

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -13,11 +13,22 @@ const counterReducer = (state = initialState, action) => {
         good: state.good + 1,
       };
     case 'OK':
-      return state;
+      return {
+        ...state,
+        ok: state.ok + 1,
+      };
     case 'BAD':
-      return state;
+      return {
+        ...state,
+        bad: state.bad + 1,
+      };
     case 'ZERO':
-      return state;
+      return {
+        ...state,
+        good: 0,
+        ok: 0,
+        bad: 0,
+      };
     default:
       return state;
   }
