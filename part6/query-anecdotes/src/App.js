c427a5bebc8939b1834d8741f862c3fa9199bc85
Mailar <83369389+devmailar@users.noreply.github.com>
2023-07-13 10:40:26 +0300

    Exercise 6.20

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,56 @@
+import { useQuery } from 'react-query';
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
+
+const App = () => {
+  const { data, status, error } = useQuery('anecdotes', async () => {
+    try {
+      const res = await fetch('http://localhost:3001/anecdotes');
+      if (!res.ok) {
+        throw new Error();
+      }
+      return await res.json();
+    } catch (error) {
+      throw new Error(
+        'Anecdote service not available due to problems in server'
+      );
+    }
+  });
+
+  switch (status) {
+    case 'loading':
+      return <div>Anecdotes are loading...</div>;
+    case 'error':
+      return (
+        <div>Anecdote service not available due to problems in server</div>
+      );
+    default:
+      break;
+  }
+
+  const handleVote = (anecdote) => {
+    console.log('vote');
+  };
+
+  const anecdotes = data || [];
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
