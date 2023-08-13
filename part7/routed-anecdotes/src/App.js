261a467aca4efcc0f56e73d9ecc2cd07bc32a5f6
Mailar <83369389+devmailar@users.noreply.github.com>
2023-08-13 12:42:39 +0300

    7.4: anecdotes and hooks step1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, Route, BrowserRouter as Router, Routes, useNavigate, useParams } from 'react-router-dom';
+import { useField } from './hooks/useField';
 
 const Menu = () => {
   const padding = {
@@ -73,17 +74,16 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const navigate = useNavigate();
-
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
 
@@ -103,24 +103,21 @@ const CreateNew = (props) => {
           content
           <input
             name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            {...content}
           />
         </div>
         <div>
           author
           <input
             name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            {...author}
           />
         </div>
         <div>
           url for more info
           <input
             name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            {...info}
           />
         </div>
         <button>create</button>
